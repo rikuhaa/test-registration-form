6eb5a72b49d18d979472a1f03c3a373c6a9872dc
rahaav <rikuhaav@gmail.com>
2017-01-12 18:48:53 +0100

    Clean up ApplicationBootstrap.java
    Replaced deprecated dependency with the current version,
    removed forwarding that is not needed anymore

--- src/main/java/com/k15t/pat/ApplicationBootstrap.java
+++ src/main/java/com/k15t/pat/ApplicationBootstrap.java
@@ -4,39 +4,25 @@ import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class ApplicationBootstrap {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationBootstrap.class, args);
-    }
-
-
-    @Bean
-    public ServletRegistrationBean initJerseyServlet() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new ServletContainer(), "/rest/*");
-        registration.addInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, JerseyConfig.class.getName());
-        return registration;
-    }
-
-    @Bean
-    public WebMvcConfigurerAdapter forwardToIndex() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("forward:/registration.html");
-            }
-        };
-    }
-
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBootstrap.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean initJerseyServlet() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new ServletContainer(), "/rest/*");
+		registration.addInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, JerseyConfig.class.getName());
+		return registration;
+	}
+	
 }
