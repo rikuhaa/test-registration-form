c6f656e46b4aed545f67b7a1e404bb76a7bf9b9f
rahaav <rikuhaav@gmail.com>
2017-01-13 14:26:27 +0100

    Mark Registration bean as Spring component

--- src/main/java/com/k15t/pat/registration/Registration.java
+++ src/main/java/com/k15t/pat/registration/Registration.java
@@ -6,7 +6,9 @@ import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Registration {
 
     @NotNull
