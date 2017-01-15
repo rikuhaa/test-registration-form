6b26cdbb9128f01b5f91acc256ee6befd227bb2e
rahaav <rikuhaav@gmail.com>
2017-01-15 21:34:08 +0100

    Make UI text translatable

--- src/main/frontend/js/app.js
+++ src/main/frontend/js/app.js
@@ -23,10 +23,25 @@ angular.module('k15t-pat-registration')
     'REGISTRATION_FORM': 'Registration Form',
     'EMAIL_ALREADY_REGISTERED': 'E-Mail already registered',
     'SERVICE_NOT_AVAILABLE': 'Service not available',
-    'ERROR_IN_REGISTRATION': 'Error in registration'
+    'ERROR_IN_REGISTRATION': 'Error in registration',
+    'REGISTRATION_COMPLETED': 'Registration completed',
+    'YOU_WILL_RECEIVE_CONFIRMATION_EMAIL_SOON':
+      'You will receive confirmation e-mail soon',
+    'NAME': 'Name',
+    'PASSWORD': 'Password',
+    'REPEAT_PASSWORD': 'Repeat password',
+    'EMAIL': 'E-mail',
+    'REPEAT_EMAIL': 'Repeat e-mail',
+    'PHONE_NUMBER': 'Phone number',
+    'STREET_ADDRESS': 'Street address',
+    'CITY': 'City',
+    'COUNTRY': 'Country',
+    'ZIP_CODE': 'Zip code',
+    'REGISTER': 'Register'
   });
   $translateProvider.translations('de', {
     'REGISTRATION_FORM': 'Registrierungsformular'
+    // TODO
   });
   $translateProvider.preferredLanguage('en');
   $translateProvider.fallbackLanguage('en');
