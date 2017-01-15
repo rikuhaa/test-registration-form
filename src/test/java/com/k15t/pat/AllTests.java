b3f8e222d1efbd68cb01aee36bb0cf9865c7414d
rahaav <rikuhaav@gmail.com>
2017-01-15 18:11:49 +0100

    Add test suite for all JUnit tests

---
+++ src/test/java/com/k15t/pat/AllTests.java
@@ -0,0 +1,17 @@
+package com.k15t.pat;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.k15t.pat.registration.AddressValidationTest;
+import com.k15t.pat.registration.RegistrationDaoServiceTest;
+import com.k15t.pat.registration.RegistrationResourceTest;
+import com.k15t.pat.registration.RegistrationValidationTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ StaticContentServerTest.class, AddressValidationTest.class, RegistrationDaoServiceTest.class,
+	RegistrationResourceTest.class, RegistrationValidationTest.class })
+public class AllTests {
+
+}
